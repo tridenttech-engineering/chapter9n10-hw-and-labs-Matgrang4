a654146e88e7c9097376b65c0cc7d72180968014
Matgrang4 <matthewgranger@my.tridenttech.edu>
2024-11-15 11:40:26 -0500

    Update lab9-2.cpp

--- lab9-2.cpp
+++ lab9-2.cpp
@@ -1,5 +1,5 @@
 //Lab 9-2.cpp - displays two monthly car payments
-//Created/revised by <your name> on <current date>
+//Created/revised by <Matt Granger> on <11/15/24>
 
 #include <iostream>
 #include <cmath>
@@ -8,6 +8,28 @@ using namespace std;
 
 int main()
 {
+    // Declare an array to store test scores
+    const int SIZE = 5; // Assume there are 5 test scores
+    int testScores[SIZE];
+    int sum = 0;
+    double average;
 
+    // Input test scores
+    for (int i = 0; i < SIZE; i++) {
+        cout << "Enter score " << (i + 1) << ": ";
+        cin >> testScores[i];
+    }
+
+    // Calculate the sum of the scores
+    for (int i = 0; i < SIZE; i++) {
+        sum += testScores[i];
+    }
+
+    // Calculate the average
+    average = static_cast<double>(sum) / SIZE;
+
+    // Output the sum and average
+    cout << "The sum of the scores is: " << sum << endl;
+    cout << "The average score is: " << average << endl;
     return 0;
 }//end of main function    
